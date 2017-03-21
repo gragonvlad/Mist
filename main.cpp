13aadde8f4df7656d904cd80581c5ecebdce9da0
Sanix Darker <sanix000@yahoo.fr>
2017-03-21 08:48:27 +0100

    encrypt method and another was added

--- main.cpp
+++ main.cpp
@@ -205,25 +205,202 @@ int main(int argc, char *argv[]){
     in.close();
 }
 */
+
+/*
+  Example of how to write binary stuff in C++
+  By Darker
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main( int argc, char ** argv)
+{
+  string X("This is the message");
+  unsigned Y = 11;
+  vector<double> Z(10,1.0);
+
+  // L'ecriture
+  ofstream o("binary_out_test.bin",ios_base::binary);
+  //To write a string, print out its length, and then the char *
+  //However, X.size() is a temp, and you cannot write a temp,
+  //so you need a new variable.
+  unsigned Xsize = X.size();
+  //The next line is how you write most types:  cast a pointer to a type to a char * and give sizeof the type
+  o.write ( reinterpret_cast<char *>(&Xsize),sizeof(unsigned) );
+  o.write ( X.c_str(), Xsize*sizeof(char) );
+
+  //Write out that vector
+
+  //Take advantages of the fact that &vector<T>[0] is a pointer to type T.
+  //gotta write out the size first!!!
+  Xsize=Z.size();
+  o.write ( reinterpret_cast<char *>(&Xsize),sizeof(unsigned) );
+  o.write( reinterpret_cast<char *>(&Z[0]), Z.size()*sizeof(double) );
+  o.close();
+
+  // La lecture
+  //Now, read it all back in
+  ifstream in("binary_out_test.bin",ios_base::binary);
+
+  unsigned stringsize;
+  in.read( reinterpret_cast<char *>(&stringsize),sizeof(unsigned) );
+
+  //Cannot read to a string directly, must have buffer intermediate
+  vector<char> temp(stringsize);
+  in.read( reinterpret_cast<char *>(&temp[0]), stringsize*sizeof(char) );
+  string thestring(temp.begin(),temp.end());
+
+  unsigned vsize;
+  in.read( reinterpret_cast<char *>(&vsize),sizeof(unsigned) );
+  vector<double> thedoubles(vsize);
+  in.read( reinterpret_cast<char *>(&thedoubles[0]), vsize*sizeof(double) );
+
+  cout << stringsize << ' ' << thestring << '\n'
+       << vsize << ' ';
+  for(unsigned i = 0 ; i < thedoubles.size() ; ++i )
+    {
+      cout << thedoubles[i] << ' ';
+    }
+  cout << '\n';
+  in.close();
+}
+*/
+
 #include<iostream>
 #include <fstream>
 #include <ctype.h>
 #include <iomanip>
 #include <stdio.h>
+#include <sstream>
+
+#include "sha1.h"
 using namespace std;
 
+// Prototypes des methodes
+void welcome();
+string SHA512_encrypt(string value);
+void create_write(string filename, string mot);
+void forward_write(string filename, string mot);
+string read_file(string filename);
+string ToHex(const string& s, bool upper_case);
+
+struct bloc{ // Structure d'un bloc
+    string nom_bloc; // nom du fichier bloc
+    string hash_code_fichier; // code d'identification de l'origine du fichier
+    unsigned int ordre_bloc;
+    float taille_bloc;
+    string date_creation_bloc;
+};
+
+struct file_descript{ // Structure d'un descripteur de fichier
+    int nbre_blocs;
+    string nom_file_descript;
+    string hash_code_fichier;
+};
+
 int main(){
-    string authentificatedvalue="sanix123";
-    cout<<"Welcome to Mist... by S@n1X D4rk3r"<<endl;
-    cout<<">>Authentification(if you don't have one write no):";
+    string authentificatedvalue="", MistID="";
+    welcome();
+    //cout<<"Mist loaded..."<<endl;
+    cout<<">>MistID(if you don't have one write no):";
     cin>>authentificatedvalue;
     if(authentificatedvalue=="no"){ //if the one don't have authentifiacation ID for Mist
-        //We verify first if an ID don't exist
-        cout<<"Create your AuthentificationID!\n >>MistID:";
+        //We verify first if an ID don't exist in a file with the SHA1 Crypt ID (l'ordre de cryptage devra etre aleatoire en fonction de chaque creation de son MistID)
+
+        cout<<"\nYou need to create your MistID first!\n>>MistID:";
         cin>>MistID;
+        cout << SHA512_encrypt(MistID)<<endl;
     }else{
         //We take the value in the file
         //We test the ID
     }
     return 0;
+}
+
+
+//Pour la bienvenue
+void welcome(){
+    cout<<"\n\t|--------------------------------------------------------------|\n";
+    cout<<"\t|-------------------- WelCome to MIST -------------------------|\n";
+    cout<<"\t|---------------------------------------------by S@n1X D4rk3r--|\n\n";
+}
+
+//For creating the file
+void create_write(string filename, string mot){
+    ofstream file;
+    if(file){
+        file.open(filename.c_str());//ou ,ios::app avec la chaine de caracteree
+        cout << "writting...."<<endl;
+        file<<mot; file.close();
+    }else
+        cout <<"Error in opening file!";
+}
+
+//For forward the file
+void forward_write(string filename, string mot){
+    ofstream file;
+    if(file){
+        file.open(filename,ios::app);//ou ,ios::app avec la chaine de caracteree
+        cout << "writting...."<<endl;
+        file<<mot<<endl; file.close();
+    }else
+        cout <<"Error in opening file!";
+}
+
+string read_file(string filename){
+    ifstream::pos_type t_size;
+    ifstream file_ (filename, ios::in|ios::binary|ios::ate);
+    string ligne(""), tohexed("");
+    char * memblock;
+    if(file_){
+        int t_size = file_.tellg();
+        memblock = new char [t_size];
+        file_.seekg (0, ios::beg);
+        file_.read (memblock, t_size);
+        file_.close();
+        tohexed = ToHex(std::string(memblock, memblock+t_size), true);
+        //tohexed = ToHex(memblock, true);
+    }else
+        cout <<"Error in opening file!";
+    return tohexed;
+}
+
+string ToHex(const string& s, bool upper_case)
+{
+    ostringstream ret;
+    for (string::size_type i = 0; i < s.length(); ++i)
+        ret << hex << setfill('0') << setw(2) << (upper_case ? uppercase : nouppercase) << (int)s[i];
+    return ret.str();
+}
+
+//Methode PRICIPALES pour MISTER un fichier ou un dossier
+void Mist(){
+    string chemin="";
+    cout<<"\nEntrez le chemin correct du fichier a mettre sur Mist!";
+    cout<<"\n>>";
+    cin>>chemin;
+}
+//Methode PRICIPALES pour DISMISTER un fichier ou un dossier
+void DisMist(){
+
+}
+
+string SHA512_encrypt(string value){
+    return  sha512(value);
+}
+
+static inline void loadbar(unsigned int x, unsigned int n, unsigned int w = 50)
+{
+    if ( (x != n) && (x % (n/100+1) != 0) ) return;
+
+    float ratio  =  x/(float)n;
+    int   c      =  ratio * w;
+
+    cout << setw(3) << (int)(ratio*100) << "% [";
+    for (int x=0; x<signed(c); x++) cout << "=";
+    for (int x=c; x<signed(w); x++) cout << " ";
+    cout << "]\r" << flush;
 }
